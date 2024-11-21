2a27d557623f9cdca4ebff774b6e3ac94de00620
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-11-21 09:33:19 -0700

    Dev #50

--- Documentation/RELEASE_NOTES_0.32.0.md
+++ Documentation/RELEASE_NOTES_0.32.0.md
@@ -4,4 +4,5 @@
 * TS::pull() supports both LIFO and FIFO policies.
 * Function ```TS::fold_all_in_current()``` folding all data in stack into list, including NODATA elements
 * Functions TS::fold_current() and TS::fold_stack() do recognize NODATA and stop folding if NODATA is found in stack
-* Function ```ts_move_to_current_stack()``` Moving last data from named datack to current stack
+* Function ```ts_move_to_current_stack()``` Moving elements from named datack to current stack
+* Functions ```TS::[move_from_current()|move_to_current()|move_from_stack()]``` recognize NODATA and stop moving if found.
--- src/ts_move.rs
+++ src/ts_move.rs
@@ -1,4 +1,5 @@
 use crate::ts::TS;
+use rust_dynamic::types::NODATA;
 
 pub fn ts_move_from_current(ts: &mut TS, name_to: String) -> &mut TS {
     ts.move_from_current(name_to)
@@ -9,13 +10,7 @@ pub fn ts_move_from_stack(ts: &mut TS, name_from: String, name_to: String) -> &m
 }
 
 pub fn ts_move_to_current_stack(ts: &mut TS, name_from: String) -> &mut TS {
-    let stack_name = match ts.current_stack_name() {
-        Some(stack_name) => stack_name,
-        None => {
-            return ts;
-        }
-    };
-    ts.move_from_stack(name_from, stack_name)
+    ts.move_to_current(name_from)
 }
 
 impl TS {
@@ -23,7 +18,32 @@ impl TS {
         loop {
             match self.pull() {
                 Some(value) => {
+                    if value.type_of() == NODATA {
+                        break;
+                    }
                     self.push_to_stack(name_to.clone(), value);
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+        self
+    }
+    pub fn move_to_current(&mut self, name_from: String) -> &mut TS {
+        let stack_name = match ts.current_stack_name() {
+            Some(stack_name) => stack_name,
+            None => {
+                return ts;
+            }
+        };
+        loop {
+            match self.pull_from_stack(name_from.clone()) {
+                Some(value) => {
+                    if value.type_of() == NODATA {
+                        break;
+                    }
+                    self.push_to_stack(stack_name.clone(), value);
                 }
                 None => {
                     break;
@@ -36,6 +56,9 @@ impl TS {
         loop {
             match self.pull_from_stack(name_from.clone()) {
                 Some(value) => {
+                    if value.type_of() == NODATA {
+                        break;
+                    }
                     self.push_to_stack(name_to.clone(), value);
                 }
                 None => {
